30525ec82d1606d48b156d618f44b863c35d41bf
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2024-08-02 15:09:14 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -352,7 +352,7 @@ const Navbar = () => {
                           </li>
                           <li>
                             <a
-                              href="/"
+                              href="https://ethaum-dashboard-frontend.vercel.app/"
                               class="text-gray-600 bg-blue-gradient rounded-lg my-2 text-center hover:text-gray-800 py-1 block font-normal"
                             >
                               Dashboard
@@ -432,7 +432,7 @@ const Navbar = () => {
                             </li>
                             <li>
                               <a
-                                href="/"
+                                href="https://ethaum-dashboard-frontend.vercel.app/"
                                 class="text-gray-600 bg-blue-gradient rounded-lg my-2 text-center hover:text-gray-800 py-1 block font-normal"
                               >
                                 Dashboard
